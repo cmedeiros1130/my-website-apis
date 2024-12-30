16b064376aafa740e2085da15646933fb192e693
cmedeiros1130 <cmedeiros1130@gmail.com>
2024-12-30 12:30:54 -0500

    Initial commit with new project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import Navbar from "./components/Navbar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./App.css";
-import Home from "./components/pages/Home";
-import Services from "./components/pages/Services";
-import Products from "./components/pages/Products";
-import SignUP from "./components/pages/Signup";
+import Accordion from "./components/Accordion";
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/sign-up" element={<SignUP />} />
-      </Routes>
-    </Router>
+    <>
+      <Accordion />
+    </>
   );
 }
 
